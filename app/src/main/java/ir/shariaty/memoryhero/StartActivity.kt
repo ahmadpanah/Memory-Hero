7574c037c8302b90703ae09bc6f7dee3f3b4dfbb
Hossein <djalicrt@gmail.com>
2024-10-16 14:25:55 +0330

    Intent from Start Activity to Main Activity

--- app/src/main/java/ir/shariaty/memoryhero/StartActivity.kt
+++ app/src/main/java/ir/shariaty/memoryhero/StartActivity.kt
@@ -1,20 +1,25 @@
 package ir.shariaty.memoryhero
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.content.IntentCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import ir.shariaty.memoryhero.databinding.ActivityStartBinding
 
 class StartActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityStartBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_start)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityStartBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnStart.setOnClickListener{
+            startActivity(Intent(this,MainActivity::class.java))
         }
     }
 }
