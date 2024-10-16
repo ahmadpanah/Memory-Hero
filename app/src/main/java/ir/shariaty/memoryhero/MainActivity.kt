e1d6057f46aa8aa5692edf2044b623d24b1695fc
Hossein <djalicrt@gmail.com>
2024-10-16 11:53:00 +0330

    finish UI and XML Design

---
+++ app/src/main/java/ir/shariaty/memoryhero/MainActivity.kt
@@ -0,0 +1,16 @@
+package ir.shariaty.memoryhero
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import ir.shariaty.memoryhero.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+}
