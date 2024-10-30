ffa3acb111a79eb2ff428b80fb6ee717fa33272c
Hossein <djalicrt@gmail.com>
2024-10-30 12:28:33 +0330

    add LifeCycle Library via gradle add Default and Yellow and Lose panel Logic

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,12 @@ dependencies {
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.androidx.activity)
+    implementation(libs.androidx.material3.android)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+
 }
--- app/src/main/java/ir/shariaty/memoryhero/MainActivity.kt
+++ app/src/main/java/ir/shariaty/memoryhero/MainActivity.kt
@@ -9,9 +9,17 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.view.View.OnClickListener
+import androidx.appcompat.widget.AppCompatButton
+import androidx.core.app.ActivityCompat
+import androidx.core.view.forEach
+import androidx.lifecycle.lifecycleScope
 import ir.shariaty.memoryhero.databinding.ActivityMainBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),OnClickListener {
     private lateinit var binding : ActivityMainBinding
     private var score = 0
     private var result : String = ""
@@ -22,5 +30,84 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
+
+        // init Views
+        binding.apply {
+            panel1.setOnClickListener(this@MainActivity)
+            panel2.setOnClickListener(this@MainActivity)
+            panel3.setOnClickListener(this@MainActivity)
+            panel4.setOnClickListener(this@MainActivity)
+            startGame()
+        }
+    }
+
+    override fun onClick(view: View?) {
+        TODO("Not yet implemented")
+    }
+
+
+    private fun disableButtons(){
+        binding.root.forEach { view ->
+            if (view is AppCompatButton)
+            {
+                view.isEnabled = false
+            }
+        }
+    }
+
+    private fun enableButtons(){
+        binding.root.forEach { view ->
+            if (view is AppCompatButton)
+            {
+                view.isEnabled = true
+            }
+        }
+    }
+
+    private fun startGame(){
+        result = ""
+        userAnswer = ""
+        disableButtons()
+        lifecycleScope.launch {
+            val round = (3 .. 5).random()
+            repeat(round)
+            {
+                delay(400)
+                val randomPanel = (1 .. 4).random()
+                result += randomPanel
+                var panel = when(randomPanel) {
+                    1 -> binding.panel1
+                    2 -> binding.panel2
+                    3 -> binding.panel3
+                    else -> binding.panel4
+                }
+                val drawbleYellow = ActivityCompat.getDrawable(this@MainActivity,R.drawable.btn_yellow)
+                val drawableDefault = ActivityCompat.getDrawable(this@MainActivity,R.drawable.btn_state)
+                panel.background = drawbleYellow
+                delay(1000)
+                panel.background = drawableDefault
+            }
+            enableButtons()
+        }
+    }
+    private fun loseAnimation(){
+        binding.apply {
+            score = 0
+            tvScore.text = "0"
+            disableButtons()
+            val drawbleLose = ActivityCompat.getDrawable(this@MainActivity,R.drawable.btn_lose)
+            val drawableDefault = ActivityCompat.getDrawable(this@MainActivity,R.drawable.btn_state)
+            lifecycleScope.launch {
+                binding.root.forEach { view ->
+                    if (view is AppCompatButton) {
+                        view.background = drawbleLose
+                        delay(300)
+                        view.background = drawableDefault
+                    }
+                }
+                delay(1000)
+                startGame()
+            }
+        }
     }
 }
